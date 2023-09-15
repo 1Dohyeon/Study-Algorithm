c370c14d81ca610e48c1ece32dc039914d4c14f5
1Dohyeon <dh1072005@gmail.com>
2023-09-15 12:50:07 +0900

    여러 기본 정렬들의 코드 작성
    코드를 작성하고 readme를 이용하여 코드 설명을 추가

--- whatever/sort/basic/Insertion.java
+++ whatever/sort/basic/Insertion.java
@@ -1,4 +1,30 @@
 package sort.basic;
 
 public class Insertion {
+    public static void main(String[] args) {
+        int[] arr = {5, 2, 9, 3, 4, 6};
+        insertionSort(arr);
+
+        System.out.println("정렬된 배열:");
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+    }
+
+    public static void insertionSort(int[] arr) {
+        int n = arr.length;
+
+        for (int i = 1; i < n; i++) {
+            int key = arr[i];
+            int j = i - 1;
+
+            // key를 적절한 위치에 삽입
+            while (j >= 0 && arr[j] > key) {
+                arr[j + 1] = arr[j];
+                j--;
+            }
+
+            arr[j + 1] = key;
+        }
+    }
 }
