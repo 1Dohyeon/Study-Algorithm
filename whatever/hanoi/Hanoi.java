6450eed63fb3b2edbfe58197bca7e0648f930a05
1Dohyeon <dh1072005@gmail.com>
2023-09-15 12:25:13 +0900

    하노이 탑 알고리즘 코드와 설명 추가

--- whatever/hanoi/Hanoi.java
+++ whatever/hanoi/Hanoi.java
@@ -1,4 +1,30 @@
 package hanoi;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Hanoi {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine()); // 원판 개수
+        char A = 'A'; // 출발 기둥
+        char C = 'C'; // 도착 기둥
+        char B = 'B'; // 임시 기둥
+
+        hanoi(n, A, C, B); // A를 출발 기둥, C를 도착 기둥으로 지정
+    }
+
+    public static void hanoi(int n, char from, char to, char temp) {
+        //                     (원판 갯수, 출발 기둥, 도착 기둥, 임시 기둥)
+        if (n == 1) {
+            System.out.println("Move Disk 1 " + from + " to " + to);
+        } else {
+            hanoi(n-1, from, temp, to);
+            System.out.println("Move Disk " + n + " " + from + " to " + to);
+            hanoi(n-1, temp, to, from);
+        }
+    }
 }
