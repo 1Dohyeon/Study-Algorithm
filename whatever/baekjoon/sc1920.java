472bf39d1e663cbfe8f0742b817847f8585a88aa
1Dohyeon <dh1072005@gmail.com>
2023-10-06 22:43:04 +0900

    코드 수정

--- whatever/baekjoon/sc1920.java
+++ whatever/baekjoon/sc1920.java
@@ -4,68 +4,67 @@ import java.util.Scanner;
 import java.util.Arrays;
 
 public class sc1920 {
-
     public static void main(String[] args) {
 
-        Scanner in = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
 
-        int N = in.nextInt();
+        int N = sc.nextInt();
         int[] arr = new int[N];
 
-
         for(int i = 0; i < N; i++) {
-            arr[i] = in.nextInt();
+            arr[i] = sc.nextInt();
         }
 
+        // Arrays 클래스 내에 있는 sort(dual quick sort) 메서드로 정렬
         Arrays.sort(arr);
 
-        int M = in.nextInt();
-        StringBuilder sb = new StringBuilder();
+        int M = sc.nextInt();
         for(int i = 0; i < M; i++) {
-
-            // 찾고자 하는 값이 있을 경우 1, 없을 경우 0을 출력해야한다.
-            if(binarySearch(arr, in.nextInt()) >= 0) {
-                sb.append(1).append('\n');
-            }
-            else {
-                sb.append(0).append('\n');
-            }
+            System.out.println(binarySearch(arr, sc.nextInt(), 0, arr.length - 1));
         }
-        System.out.println(sb);
+
+        sc.close();
+
+        /**
+         * BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+         int N = Integer.parseInt(br.readLine());
+         int[] arr = new int[N];
+
+         String[] input = br.readLine().split(" ");
+         for (int i = 0; i < N; i++) {
+         arr[i] = Integer.parseInt(input[i]);
+         }
+
+         // Arrays 클래스 내에 있는 sort(dual quick sort) 메서드로 정렬
+         Arrays.sort(arr);
+
+         int M = Integer.parseInt(br.readLine());
+         input = br.readLine().split(" ");
+         for (int i = 0; i < M; i++) {
+         int target = Integer.parseInt(input[i]);
+         System.out.println(binarySearch(arr, target, 0, arr.length - 1));
+         }
+
+         br.close();
+
+         main에 위 코드처럼 bufferedreader로 입력받으면 더 빠름.*/
     }
 
+    /** arr의 start ~ end 범위에서 value가 있는지 재귀 호출을 이용하여 이진탐색으로 찾음.*/
+    public static int binarySearch(int[] arr, int value, int start, int end) {
 
-    /**
-     * @param arr 정렬 된 배열
-     * @param value 찾으려는 값
-     * @return key와 일치하는 배열의 인덱스
-     */
-    public static int binarySearch(int[] arr, int value) {
-
-        int lo = 0;					// 탐색 범위의 왼쪽 끝 인덱스
-        int hi = arr.length - 1;	// 탐색 범위의 오른쪽 끝 인덱스
-
-        // lo가 hi보다 커지기 전까지 반복한다.
-        while(lo <= hi) {
-
-            int mid = (lo + hi) / 2;	// 중간위치를 구한다.
-
-            // key값이 중간 위치의 값보다 작을 경우
-            if(value < arr[mid]) {
-                hi = mid - 1;
+        if (start <= end) {
+            int mid = start + (end - start) / 2;
+            if (arr[mid] == value) {
+                return 1;
             }
-            // key값이 중간 위치의 값보다 클 경우
-            else if(value > arr[mid]) {
-                lo = mid + 1;
-            }
-            // key값과 중간 위치의 값이 같을 경우
-            else {
-                return mid;
+            if (arr[mid] < value) {
+                return binarySearch(arr, value, mid + 1, end);
+            } else {
+                return binarySearch(arr, value, start, mid - 1);
             }
         }
-
-        // 찾고자 하는 값이 존재하지 않을 경우
-        return -1;
-
+        return 0;
     }
 }
