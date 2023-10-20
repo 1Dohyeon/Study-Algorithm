17ae6c1a6acfe78cccc5a98a7e11b849953d77e5
1Dohyeon <dh1072005@gmail.com>
2023-10-20 23:19:15 +0900

    다이나믹 프로그래밍 알고리즘 코드 파일 생성

---
+++ whatever/baekjoon/DynamicProgramming/sc2579.java
@@ -0,0 +1,11 @@
+package baekjoon.DynamicProgramming;
+
+public class sc2579 {
+    public static void main(String[] args) {
+
+    }
+
+    private static int solution(int N) {
+        return N;
+    }
+}
