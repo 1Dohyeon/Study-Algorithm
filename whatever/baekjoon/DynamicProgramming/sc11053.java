80e6b45845c8ee72f24409725639e820c21fea07
1Dohyeon <dh1072005@gmail.com>
2023-10-21 00:26:14 +0900

    다이나믹 알고리즘 코드 작성

--- whatever/baekjoon/DynamicProgramming/sc11053.java
+++ whatever/baekjoon/DynamicProgramming/sc11053.java
@@ -1,11 +1,43 @@
 package baekjoon.DynamicProgramming;
 
+import java.util.Scanner;
+
 public class sc11053 {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+
+        for(int i=0; i<n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        System.out.println(solution(arr, n));
+        sc.close();
     }
 
-    private static int solution(int N) {
-        return N;
+    private static int solution(int[] arr, int n) {
+        int[] dp = new int[n];
+        dp[0] = 1;
+
+        for (int i = 1; i < n; i++) {
+            dp[i] = 1; // 최소 길이는 1
+
+            for (int j = 0; j < i; j++) {
+                if (arr[i] > arr[j] && dp[i] < dp[j] + 1) {
+                    dp[i] = dp[j] + 1;
+                }
+            }
+        }
+
+        int max = 0;
+        for (int i = 0; i < n; i++) {
+            if (dp[i] > max) {
+                max = dp[i];
+            }
+        }
+
+        return max;
     }
 }
