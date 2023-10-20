cfa7d35cf5453dca70ddf4cc5638db963a1ffc93
1Dohyeon <dh1072005@gmail.com>
2023-10-20 22:11:21 +0900

    다이나믹 프로그래밍 알고리즘 추가

---
+++ whatever/baekjoon/DynamicProgramming/sc1932.java
@@ -0,0 +1,4 @@
+package baekjoon.DynamicProgramming;
+
+public class sc1932 {
+}
--- whatever/baekjoon/DynamicProgramming/sc2839.java
+++ whatever/baekjoon/DynamicProgramming/sc2839.java
@@ -1,4 +1,42 @@
 package baekjoon.DynamicProgramming;
 
+import java.util.Scanner;
+
 public class sc2839 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+
+        System.out.println(solution(N));
+    }
+
+    private static int solution(int N) {
+        int[] dp = new int[N + 1];
+
+        if (N >= 3)
+            dp[3] = 1;
+        if (N >= 5)
+            dp[5] = 1;
+        if(N>5){
+            for(int i=6; i<=N; i++) {
+                if(i%5==0) {
+                    dp[i] = dp[i-5] + 1;
+                } else if (i%3==0) {
+                    dp[i] = dp[i-3] + 1;
+                } else {
+                    if(dp[i-3]!=0 && dp[i-5]!=0) {
+                        dp[i] = Math.min(dp[i-3], dp[i-5]) + 1;
+                    }
+                }
+            }
+        }
+
+        if(dp[N]==0) {
+            return -1;
+        }
+        return dp[N];
+    }
+
 }
