615639fede5a8a9f23c35d5b4cb79b472bd88207
1Dohyeon <dh1072005@gmail.com>
2023-10-20 22:37:29 +0900

    코드 수정 및 작성

--- whatever/baekjoon/DynamicProgramming/sc11727.java
+++ whatever/baekjoon/DynamicProgramming/sc11727.java
@@ -1,11 +1,30 @@
 package baekjoon.DynamicProgramming;
 
+import java.util.Scanner;
+
 public class sc11727 {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
 
+        System.out.println(solution(n));
+        sc.close();
     }
 
     private static int solution(int N) {
-        return N;
+        int[] dp = new int[1001];
+        dp[1] = 1;
+        dp[2] = 3;
+
+        /**
+         * i-1 인 경우에서는 세로로 긴 타일을 놓는 경우 1개만 허용됨.
+         * i-2 인 경우에서는 가로로 긴 타일 2개를 놓는 경우와
+         * 2x2 크기의 타일을 놓는 경우 두개로 나뉘어짐.
+         * 여기서 세로로 긴 타일 2개를 안놓는 이유는 i-1의 경우와 중복되기 때문.*/
+        for(int i=3; i<N+1; i++) {
+            dp[i] = (2*dp[i-2] + dp[i-1])%10007;
+        }
+
+        return dp[N];
     }
 }
--- whatever/baekjoon/DynamicProgramming/sc2839.java
+++ whatever/baekjoon/DynamicProgramming/sc2839.java
@@ -6,10 +6,10 @@ public class sc2839 {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
-
         int N = sc.nextInt();
 
         System.out.println(solution(N));
+        sc.close();
     }
 
     private static int solution(int N) {
