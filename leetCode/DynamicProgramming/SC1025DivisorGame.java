53ae4e5affa9fcb1b3de9968aff40d249bc4f9b4
1Dohyeon <dh1072005@gmail.com>
2023-10-28 16:26:30 +0900

    코드 설명 추가

--- leetCode/DynamicProgramming/SC1025DivisorGame.java
+++ leetCode/DynamicProgramming/SC1025DivisorGame.java
@@ -9,12 +9,13 @@ public class SC1025DivisorGame {
         // dp[i]는 숫자 i에서 게임을 시작했을 때 Alice가 이길 수 있는지 나타내는 배열
         boolean[] dp = new boolean[n + 1];
 
-        // dp[0], dp[1] 는 dp의 기본 값인 false
+        // dp[0], dp[1] 는 기본값 false
         for (int i=2; i<=n; i++) {
             // Alice의 차례에서, 가능한 모든 x에 대해 검사
             for (int x=1; x<i; x++) {
-                // 만약 x로 나누어지고 다음 차례에서 Bob이 이기는 경우가 있다면,
-                // Alice는 현재 상태에서 이길 수 있음
+                // 만약 x로 나누어지고 i-x에서 Alice가 졌다면,
+                // Bob 또한 질 수밖에 없다 => 둘 다 최선을 다하기 때문.
+                // 즉, dp[i-x]가 false 라면 dp[i]는 true 가 될 수밖에 없다.
                 if (i % x == 0 && !dp[i-x]) { // dp[i-x]==false ==> !dp[i-x]
                     dp[i] = true;
                     break; // 더 이상 검사할 필요 없음
